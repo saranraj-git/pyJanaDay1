440108425bbfc68296059c51a22146e8e05cd401
saran <47510467+svenugopal333@users.noreply.github.com>
2020-04-17 22:02:10 +1000

    initial hello world program by saran

---
+++ methods1.py
@@ -0,0 +1,10 @@
+def student(fname, lname, age):
+    print(“student first name is ”, fname)
+    print(“student last name is”, lname)
+    print(“student age is ”, age)
+
+
+student(”jana”, “prasanna”, 18)
+# student(”saran”, “raj”, 33)
+# student(”danny”, “dan”, 3)
+# student(”rocky”, “rock”, 1)
