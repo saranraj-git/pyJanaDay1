5e8bc30d0eb25eae77b3880cd8a5f5cfc5e772a1
saran <47510467+svenugopal333@users.noreply.github.com>
2020-04-26 16:52:21 +1000

    Library_Management.py

--- Library_Management.py
+++ Library_Management.py
@@ -26,3 +26,16 @@ get_books(search)
 rbook = str(input("Enter book name to remove: "))
 remove_books(rbook)
 get_books(rbook)
+
+'''
+/usr/local/bin/python3.8 /Users/saran/PycharmProjects/pyJanaDay1/Library_Management.py
+Enter book name: science
+Enter science qty: 4
+Enter book name to search: science
+science book found in library
+Enter book name to remove: maths
+maths book not found in library
+
+Process finished with exit code 0
+
+'''
